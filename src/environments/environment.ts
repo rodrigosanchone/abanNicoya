ec1f36f4dadf67896c5ec7143438e95354d5fa38
rodrigosanchonet <80555128+rodrigosanchone@users.noreply.github.com>
2022-06-24 17:21:59 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,19 +5,7 @@
 export const environment = {
   production: true,
   firestore:{
-    apiKey: "AIzaSyDJf0TLEoq3ZbP4Kobbw1Jw6cxKyp-d47Y",
-
-    authDomain: "abannicoya.firebaseapp.com",
   
-    projectId: "abannicoya",
-  
-    storageBucket: "abannicoya.appspot.com",
-  
-    messagingSenderId: "906150018774",
-  
-    appId: "1:906150018774:web:3508c41a165e8bfa88d377",
-  
-    measurementId: "G-C3P8BM9KL2"
   
   }
 };
